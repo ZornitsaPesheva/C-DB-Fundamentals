0e81568563cee01f0c2ba65eedb295f099c6dd7d
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-01-16 15:01:07 +0200

    SoftUni

--- Databases Basics - MS SQL Server/SoftUni.sql
+++ Databases Basics - MS SQL Server/SoftUni.sql
@@ -97,3 +97,30 @@ SELECT * FROM Employees
 
 --20.
 
+SELECT * FROM Towns
+ORDER BY Name
+
+SELECT * FROM Departments
+ORDER BY Name
+
+SELECT * FROM Employees
+ORDER BY Salary DESC
+
+--21.
+
+SELECT Name FROM Towns
+ORDER BY Name
+
+SELECT Name FROM Departments
+ORDER BY Name
+
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+ORDER BY Salary DESC
+
+--22.
+
+UPDATE Employees
+SET Salary = Salary + Salary * 10 / 100
+
+SELECT Salary from Employees
+
