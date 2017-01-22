08deccd278c70e6d453aa6b3d0d382c5ad2bbc61
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-01-22 15:02:26 +0200

    build_in_functions

--- Databases Basics - MS SQL Server/Built-in Functions.sql
+++ Databases Basics - MS SQL Server/Built-in Functions.sql
@@ -5,3 +5,19 @@ WHERE LEFT(FirstName, 2) = 'SA'
 --2
 SELECT FirstName, LastName FROM Employees
 WHERE LastName LIKE '%ei%'
+
+--3
+SELECT FirstName FROM Employees
+WHERE DepartmentID IN (3, 10)  AND YEAR([HireDate]) BETWEEN 1995 AND 2005
+
+--4
+SELECT FirstName, LastName FROM Employees
+WHERE JobTitle NOT LIKE '%engineer%'
+
+--5
+SELECT Name FROM Towns
+WHERE LEN(Name) IN (5, 6)
+ORDER BY Name 
+
+--6
+
