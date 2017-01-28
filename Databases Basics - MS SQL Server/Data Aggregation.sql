5d9ffe912a161e4430cad439bd320d533a7e2fa5
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-01-28 18:42:18 +0200

    data aggregation

--- Databases Basics - MS SQL Server/Data Aggregation.sql
+++ Databases Basics - MS SQL Server/Data Aggregation.sql
@@ -27,3 +27,58 @@ SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum FROM WizzardDeposits
 GROUP BY [DepositGroup]
 
 --6
+SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY [DepositGroup]
+
+--7
+SELECT [DepositGroup], SUM([DepositAmount]) AS TotalSum FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY [DepositGroup]
+HAVING SUM([DepositAmount]) < 150000
+ORDER BY TotalSum DESC
+
+--8
+SELECT [DepositGroup], [MagicWandCreator], MIN(DepositCharge) FROM WizzardDeposits
+GROUP BY [DepositGroup], [MagicWandCreator]
+ORDER BY [MagicWandCreator], [DepositGroup]
+
+--9 
+SELECT AgeGroup =
+	CASE
+		WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END, 
+	COUNT(*) AS WizardCount
+FROM WizzardDeposits
+GROUP BY 
+	CASE
+		WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END
+
+--10
+SELECT LEFT(FirstName, 1) AS FirstLetter FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+GROUP BY LEFT(FirstName, 1)
+ORDER BY FirstLetter
+
+--11
+SELECT DepositGroup, IsDepositExpired, AVG(DepositInterest) FROM WizzardDeposits
+WHERE DepositStartDate > '1985-01-01'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
+
+--12
+
+
