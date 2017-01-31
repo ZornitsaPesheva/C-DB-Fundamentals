6f49b1c5d75566667a9b529a9cf9767d2f05d4d0
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-01-31 18:15:15 +0200

    table relations

--- Databases Basics - MS SQL Server/Table Relations.sql
+++ Databases Basics - MS SQL Server/Table Relations.sql
@@ -138,8 +138,48 @@ REFERENCES Teachers(TeacherID)
 
 -- 05. Online Store Database
 
+--CREATE DATABASE OnlineStore
 
+--USE OnlineStore
 
+CREATE TABLE Cities(
+CityID INT PRIMARY KEY,
+Name VARCHAR(50) NOT NULL)
 
+CREATE TABLE Customers(
+CustomerID INT PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+Birthday DATE,
+CityID INT NOT NULL,
+CONSTRAINT FK_Customers_Cities
+	FOREIGN KEY (CityID) REFERENCES Cities(CityID)
+)
 
+CREATE TABLE Orders(
+OrderID INT PRIMARY KEY,
+CustomerID INT NOT NULL,
+CONSTRAINT FK_Orders_Customers
+	FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
 
+CREATE TABLE ItemTypes(
+ItemTypeID INT PRIMARY KEY,
+Name VARCHAR(50) NOT NULL)
+
+CREATE TABLE Items(
+ItemID INT PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+ItemTypeID INT NOT NULL,
+CONSTRAINT FK_Items_ItemTypes
+	FOREIGN KEY (ItemTypeID) REFERENCES ItemTypes(ItemTypeID)
+)
+
+CREATE TABLE OrderItems(
+OrderID INT NOT NULL,
+ItemID INT NOT NULL,
+CONSTRAINT PK_OrderItems PRIMARY KEY (OrderID, ItemID),
+CONSTRAINT FK_OrderItems_Orders
+	FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+CONSTRAINT FK_OrderItems_Items
+	FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
+)
