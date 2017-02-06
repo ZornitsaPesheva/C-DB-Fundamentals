aa8c1960b4c87f5c8c7337bb0fb10f8eca1422ab
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-02-06 09:54:00 +0200

    Functions Triggers And Transactions

--- Databases Basics - MS SQL Server/Functions Triggers And Transactions.sql
+++ Databases Basics - MS SQL Server/Functions Triggers And Transactions.sql
@@ -7,4 +7,27 @@ BEGIN
 	WHERE Salary > 35000
 END
 
+EXEC usp_GetEmployeesSalaryAbove35000
+
 -- 02. Employees with Salary Above Number
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@number Money)
+AS
+BEGIN
+	SELECT FirstName AS 'First Name', LastName AS 'Last Name'
+	FROM Employees
+	WHERE Salary >= @number
+END
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
+
+-- 03. Town Names Starting With
+CREATE PROCEDURE usp_GetTownsStartingWith (@parameter NVARCHAR(30))
+AS
+BEGIN
+	SELECT Name AS Town FROM Towns
+	WHERE Name LIKE @parameter + '%'
+END
+
+EXEC usp_GetTownsStartingWith b
+
+-- 04. Employees from Town
