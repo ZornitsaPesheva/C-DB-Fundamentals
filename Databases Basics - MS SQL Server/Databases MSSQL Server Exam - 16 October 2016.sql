87598a57f5b91ae5c93ce0a2805d5aacedeed95b
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-02-13 18:33:05 +0200

    Databases MSSQL Server Exam - 16 October 2016

--- Databases Basics - MS SQL Server/Databases MSSQL Server Exam - 16 October 2016.sql
+++ Databases Basics - MS SQL Server/Databases MSSQL Server Exam - 16 October 2016.sql
@@ -105,3 +105,42 @@ CONSTRAINT FK_CustomerBankAccounts_Customers
 )
 
 -- Section 2: DML - 05. Fillin New Tables
+INSERT INTO CustomerReviews ([ReviewID], [ReviewContent], 
+	[ReviewGrade], [AirlineID], [CustomerID])
+VAlUES
+	(1, 'Me is very happy. Me likey this airline. Me good.', 10, 1, 1),
+	(2, 'Ja, Ja, Ja... Ja, Gut, Gut, Ja Gut! Sehr Gut!', 10, 1, 4),
+	(3, 'Meh...', 5, 4, 3),
+	(4, 'Well Ive seen better, but Ive certainly seen a lot worse...', 7, 3, 5)
+
+INSERT INTO CustomerBankAccounts ([AccountID], [AccountNumber],
+	[Balance], [CustomerID])
+VALUES
+	(1, '123456790', 2569.23, 1),
+	(2, '18ABC23672', 14004568.23, 2),
+	(3, 'F0RG0100N3', 19345.20, 5)
+
+-- Section 3: Querying - 01. Extract All Tickets
+SELECT TicketID, Price, Class, Seat
+FROM Tickets
+ORDER BY TicketID
+
+-- Section 3: Querying - 02. Extract All Customers
+SELECT CustomerID, FirstName + ' ' + LastName AS FullName, Gender 
+FROM Customers
+ORDER BY FullName, CustomerID
+
+-- Section 3: Querying - 03. Extract Delayed Flights
+SELECT FlightID, DepartureTime, ArrivalTime
+FROM Flights
+WHERE Status = 'Delayed'
+ORDER BY FlightID
+
+-- Section 3: Querying - 04. Top 5 Airlines
+SELECT TOP 5 AirlineID, AirlineName, Nationality, Rating
+FROM Airlines
+WHERE AirlineID IN (SELECT AirlineID FROM Flights)
+ORDER BY Rating DESC, AirlineID
+
+-- Section 3: Querying - 05. All Tickets Below 5000
+
