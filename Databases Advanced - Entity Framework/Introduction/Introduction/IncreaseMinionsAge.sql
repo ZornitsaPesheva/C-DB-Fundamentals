1be31d42f6fa08545e24943a4379599a35474c17
ZornitsaSerbezova <zornitsa.serbezova@live.com>
2017-02-23 20:23:18 +0200

    intro

--- Databases Advanced - Entity Framework/Introduction/Introduction/IncreaseMinionsAge.sql
+++ Databases Advanced - Entity Framework/Introduction/Introduction/IncreaseMinionsAge.sql
@@ -7,8 +7,8 @@ UPDATE Minions
 SET Age = Age+1
 WHERE charindex(',' + CAST(Minions.Id as nvarchar(20)) + ',', @idsParamList) > 0
 
---UPDATE Minions
---SET Name = UPPER(LEFT(Name, 1) + ...) // ???
---WHERE charindex(',' + CAST(Minions.Id as nvarchar(20)) + ',', @idsParamList) > 0
+UPDATE Minions
+SET Name = UPPER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name)-1) 
+WHERE charindex(',' + CAST(Minions.Id as nvarchar(20)) + ',', @idsParamList) > 0
 
 SELECT Name, Age FROM Minions
